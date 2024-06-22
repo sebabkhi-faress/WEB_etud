4e722f36d95c909f901c0e699d7ef2a2fd8f551b
khalil <khalilcheddadi@proton.me>
2024-06-22 03:05:00 +0100

    fetch profile data function

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,22 @@ import { cookies } from "next/headers";
 import Image from "next/image";
 import axios from "axios";
 
+const getProfileData = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+  const uuid = cookieStore.get("uuid")?.value;
+
+  const profileData = await axios.get(
+    `https://progres.mesrs.dz/api/infos/bac/${uuid}/dias`,
+    {
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
+  return profileData.data;
+};
+
 const getImage = async () => {
   const cookieStore = cookies();
   const token = cookieStore.get("token")?.value;
@@ -19,6 +35,8 @@ const getImage = async () => {
 
 const Profile = async () => {
   const image = await getImage();
+  const profileData = await getProfileData();
+  console.log(profileData);
 
   return (
     <div className="bg-gray-100 w-11/12 max-w-3xl m-5 p-8 rounded-lg shadow-2xl box-border">
@@ -34,7 +52,8 @@ const Profile = async () => {
       <div className="text-left">
         <ul className="list-none p-0 m-0">
           <li className="mb-4 text-lg text-gray-800">
-            <span className="font-bold text-gray-600">First Name:</span> Ahmed
+            <span className="font-bold text-gray-600">First Name:</span>
+            {profileData.nomAr}
           </li>
           <li className="mb-4 text-lg text-gray-800">
             <span className="font-bold text-gray-600">Last Name:</span> Kassas
