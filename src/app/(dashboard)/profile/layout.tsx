295cb221e7afff489ad83ada96b9fd593b829d83
Summer Pc <0xcyborgx@gmail.com>
2024-06-28 00:21:41 +0100

    Add transition effect to profile images

--- src/app/(dashboard)/profile/layout.tsx
+++ src/app/(dashboard)/profile/layout.tsx
@@ -4,22 +4,20 @@ export const metadata = {
   title: "WebEtu - Profile",
 };
 
-const Layout = async ({
-  children,
-  image,
-  logo,
-}: {
-  children: React.ReactNode;
-  image: React.ReactNode;
-  logo: React.ReactNode;
-}) => {
+const Layout = async ({ children, image, logo }) => {
   return (
     <div className="bg-gray-300 border-2 border-green-700 w-full h-max max-w-3xl m-5 p-8 flex flex-col gap-8 rounded-lg shadow-2xl box-border">
       <div className="text-center flex flex-col sm:flex-row justify-between items-center">
-        {image}
-        {logo}
+        <div className="transition duration-300 ease-in-out transform hover:scale-105">
+          {image}
+        </div>
+        <div className="transition duration-300 ease-in-out transform hover:scale-105">
+          {logo}
+        </div>
       </div>
-      <div className="text-left">{children}</div>
+      <div className="text-left">
+        {children}
+      </div>
     </div>
   );
 };
