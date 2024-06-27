37202c0a76e04d85c17239ebbc5df8c4f98462c4
Summer Pc <0xcyborgx@gmail.com>
2024-06-27 01:39:09 +0100

    Making group UI better / adding things in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { useAuth } from "./AuthProvider";
@@ -12,11 +14,10 @@ import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const { user, signOut } = useAuth();
-
   const pathname = usePathname();
 
   return (
-    <nav className="bg-green-600 h-20 py-4 px-12 flex items-center justify-between">
+    <nav className="bg-green-600 py-4 px-4 md:px-12 flex items-center justify-between">
       <div className="flex items-center flex-shrink-0">
         <Link href="/">
           <Image
@@ -28,39 +29,49 @@ const Navbar = () => {
         </Link>
       </div>
 
-      <div className="ml-4 flex items-center md:ml-6 space-x-2">
-        {user ? (
-          pathname === "/" ? (
-            <Link
-              href="/profile"
-              className={`flex gap-2 text-white hover:bg-white hover:text-green-600 px-5 py-3 rounded-md text-lg font-medium transition-colors duration-300 ${
-                pathname !== "/" ? "hidden" : ""
-              }`}
-            >
-              <UserIcon className="h-6 w-6" />
-              Profile
-            </Link>
-          ) : (
+      <div className="hidden md:flex items-center space-x-2">
+        {user && (
+          <>
+            {pathname !== "/group" && (
+              <Link
+                href="/group" // Adjust the href for your Group page
+                className="flex gap-2 text-white hover:bg-white hover:text-green-600 px-4 py-2 rounded-md text-lg font-medium transition-colors duration-300"
+              >
+                <UserIcon className="h-6 w-6" />
+                Group
+              </Link>
+            )}
+
+            {pathname !== "/profile" && (
+              <Link
+                href="/profile"
+                className="flex gap-2 text-white hover:bg-white hover:text-green-600 px-4 py-2 rounded-md text-lg font-medium transition-colors duration-300"
+              >
+                <UserIcon className="h-6 w-6" />
+                Profile
+              </Link>
+            )}
+            
             <button
-              className="flex gap-2 text-white hover:bg-white hover:text-red-600 px-5 py-3 rounded-md text-lg font-medium transition-colors duration-300"
+              className="flex gap-2 text-white hover:bg-white hover:text-red-600 px-4 py-2 rounded-md text-lg font-medium transition-colors duration-300"
               onClick={() => signOut()}
             >
               <ArrowLeftStartOnRectangleIcon className="h-6 w-6" />
               Sign Out
             </button>
-          )
-        ) : (
-          <>
-            <Link
-              href="/login"
-              className={`flex gap-2 text-white hover:bg-white hover:text-green-600 px-5 py-3 rounded-md text-lg font-medium transition-colors duration-300 ${
-                pathname === "/login" ? "hidden" : ""
-              }`}
-            >
-              <ArrowRightEndOnRectangleIcon className="h-6 w-6" />
-              Sign In
-            </Link>
           </>
+        )}
+
+        {!user && (
+          <Link
+            href="/login"
+            className={`flex gap-2 text-white hover:bg-white hover:text-green-600 px-4 py-2 rounded-md text-lg font-medium transition-colors duration-300 ${
+              pathname === "/login" ? "hidden" : ""
+            }`}
+          >
+            <ArrowRightEndOnRectangleIcon className="h-6 w-6" />
+            Sign In
+          </Link>
         )}
       </div>
     </nav>
