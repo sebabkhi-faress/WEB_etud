14e576ddbe8cc200e5f07b2d35809111efa5b913
khalil-cheddadi <khalil3cheddadi@gmail.com>
2024-09-24 10:38:50 +0100

    logger format modfied

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,13 +1,9 @@
 const info = (message: string, user: string | undefined, path: string) => {
-  let now = new Date()
-  let formattedDate = `${now.getFullYear()}/${now.getUTCMonth()}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
-  console.info(`[${formattedDate}] [${user}] [${path}] ${message}`)
+  console.info(`[${user}] [${path}] ${message}`)
 }
 
 const error = (message: string, user: string | undefined, path: string) => {
-  let now = new Date()
-  let formattedDate = `${now.getFullYear()}/${now.getUTCMonth()}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
-  console.error(`[${formattedDate}] [${user}] [${path}] ${message}`)
+  console.error(`[${user}] [${path}] ${message}`)
 }
 
 const logger = { info, error }
