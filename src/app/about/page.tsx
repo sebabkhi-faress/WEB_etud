18b496fc8302b4272867f77b6552f65d03a572ff
0xcyborg <0xcyborgx@gmail.com>
2024-10-01 08:59:24 +0100

    Replacing 'أسماء المطورين' with 'فريق العمل'

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -76,7 +76,7 @@ const AboutPage = async () => {
 
       <section className="mb-10">
         <h2 className="text-2xl sm:text-3xl font-semibold text-green-700 mb-4">
-          أسماء المطورين
+          فريق العمل
         </h2>
         <ul className="list-disc list-inside text-gray-700 text-lg leading-relaxed space-y-2">
           <li className="pr-4">قصاص أحمد عبد النور</li>
