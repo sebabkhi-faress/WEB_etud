2543eb9784a65d2dcfb23311c5b9d5f553e39e42
0xcyborg <0xcyborgx@gmail.com>
2024-10-04 21:10:43 +0100

    Added panel loading

--- src/app/(dashboard)/panel/loading.tsx
+++ src/app/(dashboard)/panel/loading.tsx
@@ -1,7 +1,7 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline"
 
-const DiaLoading = () => {
+const PanelLoading = () => {
   return <ArrowPathIcon className="animate-spin h-16 w-16 m-5" />
 }
 
-export default DiaLoading
+export default PanelLoading
