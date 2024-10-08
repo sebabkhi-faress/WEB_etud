526d4bc848988becfaff3981472106c2e1d23cc3
0xcyborg <0xcyborgx@gmail.com>
2024-10-08 21:11:23 +0100

    Responsive loading page for profile

--- src/app/(dashboard)/profile/loading.tsx
+++ src/app/(dashboard)/profile/loading.tsx
@@ -1,7 +1,7 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline"
 export default function ProfileLoading() {
   return (
-    <div className="animate-loading border border-green-700 lg:w-full max-w-3xl flex justify-center items-center rounded shadow-2xl absolute top-5 bottom-5 right-5 left-5 lg:left-auto lg:right-auto">
+    <div className="animate-loading bg-gray-200 border items-center border-gray-400 w-full h-max max-w-3xl m-5 p-7 flex flex-col gap-8 rounded shadow-5x5 box-border">
       <ArrowPathIcon className="animate-spin h-16 w-16 m-5" />
     </div>
   )
