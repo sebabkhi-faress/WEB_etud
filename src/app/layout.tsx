19985bc6707a08872c8a0548173b469710076a2a
0xcyborg <0xcyborgx@gmail.com>
2024-10-10 22:45:38 +0100

    Adding more metadata / adding new variable in .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,38 @@
-import type { Metadata } from "next"
+import type { Metadata, Viewport } from "next"
 import "./globals.css"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import { Toaster } from "react-hot-toast"
 import Navbar from "@/components/Navbar"
 
 export const metadata: Metadata = {
-  title: "WebEtu",
-  description: "Made By OSCA",
+  title: "WebEtu - Student Portal",
+  description:
+    "WebEtu is a student portal designed for students to view their profiles, notes, and academic resources..",
+  keywords:
+    "student portal, view profiles, student notes, education, WebEtu, academic resources, progres, badji mokhtar, university",
+  authors: [{ name: "OSCA", url: process.env.CURRENT_HOST }],
+  openGraph: {
+    title: "WebEtu - Student Portal",
+    description:
+      "WebEtu provides students with easy access to their profiles, notes, and important academic resources..",
+    url: process.env.CURRENT_HOST,
+    siteName: "WebEtu",
+    images: [
+      {
+        url: "/images/logo-black.png",
+        alt: "WebEtu Logo",
+        width: 800,
+        height: 600,
+      },
+    ],
+    type: "website",
+  },
   icons: {
     icon: [
       {
         media: "(prefers-color-scheme: light)",
-        url: "/images/logo-dark.png",
-        href: "/images/logo-dark.png",
+        url: "/images/logo-black.png",
+        href: "/images/logo-black.png",
       },
       {
         media: "(prefers-color-scheme: dark)",
@@ -21,6 +41,11 @@ export const metadata: Metadata = {
       },
     ],
   },
+}
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: "device-width",
 }
 
 export default function RootLayout({
