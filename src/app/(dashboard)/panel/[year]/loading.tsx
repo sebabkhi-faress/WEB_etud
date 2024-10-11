0586b8d0ec9580292ace35dfba39f54ec09f31ac
khalil-cheddadi <khalil3cheddadi@gmail.com>
2024-10-11 21:03:10 +0100

    adding loading page to year page

---
+++ src/app/(dashboard)/panel/[year]/loading.tsx
@@ -0,0 +1,11 @@
+import { ArrowPathIcon } from "@heroicons/react/24/outline"
+
+const DiasLoading = () => {
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <ArrowPathIcon className="animate-spin h-16 w-16 m-5" />
+    </div>
+  )
+}
+
+export default DiasLoading
