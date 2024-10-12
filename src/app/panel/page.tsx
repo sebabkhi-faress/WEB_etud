5f1cf87f01f53e325334b397538f31d38ca9977c
khalil-cheddadi <khalil3cheddadi@gmail.com>
2024-10-12 20:44:18 +0100

    fixed panel page offset

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -10,8 +10,8 @@ export default async function PanelPage() {
   const currentYear = process.env.CURRENT_YEAR
 
   return (
-    <div className="flex flex-1 flex-col gap-4 max-w-5xl md:mx-12 lg:mx-24 p-5">
-      <h1 className="md:text-xl lg:text-3xl font-bold">Enrollments:</h1>
+    <div className="flex flex-1 flex-col gap-4 max-w-5xl w-full md:mx-12 lg:mx-24 p-4">
+      <h1 className="text-lg md:text-xl lg:text-3xl font-bold">Enrollments:</h1>
       <Enrollments dias={dias} currentYear={currentYear} />
     </div>
   )
